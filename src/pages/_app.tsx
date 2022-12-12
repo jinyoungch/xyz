fb0625e000ba9de4ae8703ea40fbf46291c56a68
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2022-12-12 14:56:53 -0500

    change meta description (unlikelily) one last time 🤡

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,8 +15,8 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="image" property="og:image" content="public/portugal_og-image-1200x630px.jpeg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta name="description" property="og:description" content="self-proclaimed interesting human bean 🫘" />
-        <meta property="og:description" content="self-proclaimed interesting human bean 🫘" />
+        <meta name="description" property="og:description" content="a self-proclaimed interesting human bean 🫘" />
+        <meta property="og:description" content="a self-proclaimed interesting human bean 🫘" />
         <meta property="og:type" content="website" />
         <meta name="title" property="og:title" content="jinyoung" />
         <link rel="icon" href="public/favicon.ico" />
