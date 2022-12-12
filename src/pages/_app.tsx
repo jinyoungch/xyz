8592b6543bb48439245c312148875b9479476c16
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2022-12-12 14:20:48 -0500

    added pertinent metatags for fb open graph

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,17 @@ export default function MyApp({ Component, pageProps }) {
     <ThemeProvider defaultTheme="dark" disableTransitionOnChange>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="description" content="Jin Young Choi Blog/Portfolio" />
-        <meta name="author" content="Jin Young Choi" />
+        <meta name="author" content="jinyoung" />
         {/* TODO: modify content value for meta image from relative path to canonical URL */}
-        <meta name="image" property="og:image" content="../public/portugal.jpeg" />
-        <meta name="description" property="og:description" content="(self-proclaimed) interesting person" />
+        <meta name="image" property="og:image" content="public/portugal.jpeg" />
+        <meta name="description" property="og:description" content="self-proclaimed autodidact and interesting human bean 🫘" />
+        <meta property="og:description" content="self-proclaimed autodidact and interesting human bean 🫘" />
         <meta property="og:type" content="website" />
-        <meta name="title" property="og:title" content="jinyoung choi" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" property="og:title" content="jinyoung" />
+        <link rel="icon" href="public/favicon.ico" />
         <base target="_blank" />
+        <meta property="og:url" content="https://jinyoung.xyz" />
+        <meta property="og:title" content="jinyoung" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
