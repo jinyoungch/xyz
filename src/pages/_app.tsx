e7ee269249ab43a5dcfee6c406c6c87e174f7485
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2022-12-12 16:11:36 -0500

    removing image completely for the headache it is 👹

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,8 +16,8 @@ export default function MyApp({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="jinyoung" />
         <meta property="og:description" content="a self-proclaimed interesting human bean 🫘" />
-        <meta property="og:image" content="public/portugal.jpeg" />
-        {/* <meta property="og:image:width" content="1200" />
+        {/* <meta property="og:image" content="public/portugal.jpeg" />
+        <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" /> */}
         <link rel="icon" href="public/favicon.ico" />
         <base target="_blank" />
