9aefac6dac711ac7e080d126bab8eba775cb9a03
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2022-12-14 11:46:42 -0500

    fixed relative paths & reintroduced og:image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,16 +12,19 @@ export default function MyApp({ Component, pageProps }) {
         <meta charSet="utf-8" />
         <meta name="author" content="jinyoung" />
         {/* TODO: modify content value for meta image from relative path to canonical URL */}
+        <meta name="image" property="og:image" content="portugal_og-image-1200x630px.jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:url" content="https://jinyoung.xyz" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="jinyoung" />
         <meta property="og:description" content="a self-proclaimed interesting human bean 🫘" />
+        <meta name="description" content="a self-proclaimed interesting human bean 🫘" />
         {/* <meta property="og:image" content="public/portugal.jpeg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" /> */}
-        <link rel="icon" href="public/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
         <base target="_blank" />
-        <meta property="og:title" content="jinyoung" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
