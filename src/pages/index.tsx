5828a063af6481461e96448eec39de83a1a23127
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2023-12-12 21:19:06 -0500

    update profile photo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Layout } from '../components';
-import profilePhoto from '../../public/jeju.jpeg'
+import profilePhoto from '../../public/jeju.png'
 
 export default function Index() {
   return (
